ed09c84542af9d2ae76839e1bd022885e7857872
Gunpreet Singh <gunpreet@outlook.com>
2017-06-12 19:21:19 -0700

    add basic information for models get and post

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,36 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    // get and post take a call back to execute the data 
+    get: function (callback) {
+      // write db query 
+      var dbQuery = '';
+      // use query  and pass in the call back 
+      db.query(dbQuery, function (error, result) {
+        callback(error, results);
+      });
+    }, // a function which produces all the messages
+    post: function (callback) {
+      var dbQuery = '';
+      db.query(dbQuery, function (error, result) {
+        callback(error, result);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      var dbQuery = '';
+      db.query(dbQuery, function (error, result) {
+        callback(error, results);
+      });
+    },
+    post: function (callback) {
+      var dbQuery = '';
+      db.query(dbQuery, function (error, result) {
+        callback(error, results);
+      });
+    }
   }
 };
-

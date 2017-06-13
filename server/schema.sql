1ee21caf25e7f48afff9d1065e8cf0a9757c9e94
Gunpreet Singh <gunpreet@outlook.com>
2017-06-12 17:54:45 -0700

    fix starter code for database schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,41 +1,36 @@
+DROP DATABASE if exists chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
--- ---
--- Globals
--- ---
-
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
-
--- ---
--- Table 'Messages'
--- 
--- ---
+-- add messages table
+-- add id integer 
+-- add userid 
+-- add primary key on id 
+-- add room name 
+-- add text option
 
 DROP TABLE IF EXISTS `Messages`;
 
-CREATE TABLE messages
-(
-  id int NOT NULL
-  AUTO_INCREMENT,
+CREATE TABLE messages (
+  id int NOT NULL AUTO_INCREMENT,
   userid int NOT NULL,
-  text varchar
-  (200)  NOT NULL,
-  roomname varchar
-  (20),
-  PRIMARY KEY
-  (ID)
+  text varchar(200)  NOT NULL,
+  roomname varchar(20),
+  PRIMARY KEY (ID)
 );
 
-  CREATE TABLE users
-  (
-    id int NOT NULL
-    AUTO_INCREMENT,
-  username  varchar
-    (40)   NOT NULL,
-  PRIMARY KEY
-    (ID)
+-- add user table 
+-- add id integer 
+-- add user name
+-- add primary key on id 
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username  varchar(40) NOT NULL,
+  PRIMARY KEY (ID)
 );
 

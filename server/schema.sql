51b94053759af8e3e2e347809b2e96e54ebf4b82
Gunpreet Singh <gunpreet@outlook.com>
2017-06-12 17:22:35 -0700

    update schema

--- server/schema.sql
+++ server/schema.sql
@@ -16,44 +16,26 @@ USE chat;
 
 DROP TABLE IF EXISTS `Messages`;
 
-CREATE TABLE `Messages`
+CREATE TABLE messages
 (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL
+  AUTO_INCREMENT,
   userid int NOT NULL,
   text varchar
-(200)  NOT NULL,
+  (200)  NOT NULL,
   roomname varchar
-(20),
+  (20),
   PRIMARY KEY
-(ID)
+  (ID)
 );
 
--- ---
--- Table 'Users'
--- 
--- ---
-
-DROP TABLE IF EXISTS `Users`;
-
-CREATE TABLE `Users`
- id        int    NOT NULL AUTO_INCREMENT,
+  CREATE TABLE users
+  (
+    id int NOT NULL
+    AUTO_INCREMENT,
   username  varchar
-(40)   NOT NULL,
+    (40)   NOT NULL,
   PRIMARY KEY
-(ID);
+    (ID)
+);
 
-
-
-
--- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
--- ---
--- Test Data
--- ---
-
--- INSERT INTO `Messages` (`id`,`text`,`createdAt`,`id_Users`,`id_Rooms`) VALUES
--- ('','','','','');
--- INSERT INTO `Users` (`id`,`username`) VALUES
--- ('','');

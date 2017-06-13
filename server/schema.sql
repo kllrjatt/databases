e596cd6013fbb3e9b4d57ddb2ca314594d980391
Gunpreet Singh <gunpreet@outlook.com>
2017-06-13 11:37:07 -0700

    unique usernames

--- server/schema.sql
+++ server/schema.sql
@@ -13,24 +13,33 @@ USE chat;
 
 DROP TABLE IF EXISTS `Messages`;
 
-CREATE TABLE messages (
-  id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE messages
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
   userid int NOT NULL,
-  text varchar(200)  NOT NULL,
-  roomname varchar(20),
-  PRIMARY KEY (ID)
+  text varchar
+  (200)  NOT NULL,
+  roomname varchar
+  (20),
+  PRIMARY KEY
+  (ID)
 );
 
--- add user table 
--- add id integer 
--- add user name
--- add primary key on id 
+  -- add user table 
+  -- add id integer 
+  -- add user name
+  -- add primary key on id 
 
-DROP TABLE IF EXISTS `user`;
+  DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  username  varchar(40) NOT NULL,
-  PRIMARY KEY (ID)
+  CREATE TABLE users
+  (
+    id int NOT NULL
+    AUTO_INCREMENT,
+  username  varchar
+    (40) NOT NULL UNIQUE,
+  PRIMARY KEY
+    (ID)
 );
 
